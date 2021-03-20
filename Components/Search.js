2ceb4ec95683f2d05a938d6423944cd864c5b74f
Amine Aouragh <amine.aouragh.20@gmail.com>
2021-03-20 17:34:26 +0100

    Add some templates

--- Components/Search.js
+++ Components/Search.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, Text, TextInput, Button, FlatList } from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
 
 export default class Search extends Component {
   render() {
     return (
-      <View style={{ marginTop: 40 }}>
+      <View style={styles.main_container}>
         <TextInput style={styles.textInput} placeholder="Movie Title"/>
         <Button title="Search" onPress={() => {}}/>
+        <FlatList
+          data={films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem />}
+        />
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
   textInput: {
-    marginLeft: 15,
+    marginLeft: 5,
     marginRight: 5,
     height: 50,
     paddingLeft: 5
