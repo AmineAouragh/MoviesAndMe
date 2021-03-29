8959110ca6c40f6f2175a1d2e3652b437ac1f6f1
Amine Aouragh <amine.aouragh.20@gmail.com>
2021-03-29 21:06:09 +0200

    Replaced the View with a TouchableOpacity so the container can catch onPress and display to the terminal console

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,37 +1,35 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text, Image } from 'react-native'
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
 import { getImageFromApi } from '../API/TMDBApi'
 
 export default class FilmItem extends Component {
   render() {
-    const film = this.props.film
+    const { film, displayDetailForFilm } = this.props
     return (
 
-      <View style={styles.main_container}>
+      <TouchableOpacity style={styles.main_container} onPress={() => displayDetailForFilm(film.id)}>
+        <Image
+           style={styles.image}
+           source={{uri: getImageFromApi(film.poster_path)}}
+        />
 
-         <Image
-            style={styles.image}
-            source={{uri: getImageFromApi(film.poster_path)}}
-         />
+        <View style={styles.content_container}>
 
-         <View style={styles.content_container}>
+           <View style={styles.header_container}>
+              <Text style={styles.title_text}>{film.title}</Text>
+              <Text style={styles.vote_text}>{film.vote_average}</Text>
+           </View>
 
-            <View style={styles.header_container}>
-               <Text style={styles.title_text}>{film.title}</Text>
-               <Text style={styles.vote_text}>{film.vote_average}</Text>
-            </View>
+           <View style={styles.description_container}>
+              <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
+           </View>
 
-            <View style={styles.description_container}>
-               <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
-            </View>
+           <View style={styles.date_container}>
+              <Text style={styles.date_text}>Released at {film.release_date}</Text>
+           </View>
 
-            <View style={styles.date_container}>
-               <Text style={styles.date_text}>Released at {film.release_date}</Text>
-            </View>
-
-         </View>
-
-      </View>
+        </View>
+      </TouchableOpacity>
     )
   }
 }
